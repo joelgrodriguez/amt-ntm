cd530e46702f554ecc14275900daf75c7c6ec9d9
Joel G. Rodriguez <joelgrodriguez@gmail.com>
2026-07-22 13:35:29 -0700

    Add HubSpot site-wide tracking code to <head>

--- app/header.php
+++ app/header.php
@@ -19,6 +19,9 @@ if (!defined('ABSPATH')) {
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    <!-- Start of HubSpot Embed Code -->
+    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4478417.js"></script>
+    <!-- End of HubSpot Embed Code -->
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php // Mark the document as JS-capable before paint. Scroll-reveal styles
